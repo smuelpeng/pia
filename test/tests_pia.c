1e2f8da1c783aa398f4ed3a7b8da80c3033b9a7f
J.J. Green <j.j.green@gmx.co.uk>
2015-12-02 17:34:32 +0000

    test-suite tidying

--- test/tests_pia.c
+++ test/tests_pia.c
@@ -1,6 +1,10 @@
 /*
   tests_example.h
   Copyright (c) J.J. Green 2015
+
+  This is the same as the test-suite included in the original
+  aip.c, and like it tests that the obtained results are
+  exact to float precision.
 */
 
 #include <pia.h>
@@ -26,93 +30,109 @@ extern void test_pia_self(void)
   size_t na = sizeof(a)/sizeof(point_t);
   float area = pia_area(a, na, a, na);
 
-  CU_ASSERT(area == 0.5f);
+  CU_ASSERT_EQUAL(area, 0.5f);
 }
 
 extern void test_pia_redundant(void)
 {
-  point_t a[] = {{2, 3}, {2, 3}, {2, 3}, {2, 4}, {3, 3}, {2, 3}, {2, 3}};
-  point_t b[] = {{1, 1}, {1, 4}, {4, 4}, {4, 1}, {1, 1}};
-  size_t na = sizeof(a)/sizeof(point_t);
-  size_t nb = sizeof(b)/sizeof(point_t);
+  point_t
+    a[] = {{2, 3}, {2, 3}, {2, 3}, {2, 4}, {3, 3}, {2, 3}, {2, 3}},
+    b[] = {{1, 1}, {1, 4}, {4, 4}, {4, 1}, {1, 1}};
+  size_t
+    na = sizeof(a)/sizeof(point_t),
+    nb = sizeof(b)/sizeof(point_t);
   float area = pia_area(a, na, b, nb);
-  //printf("[%.8f]", area);
-  CU_ASSERT(area == 0.5f);
+
+  CU_ASSERT_EQUAL(area, 0.5f);
 }
 
 extern void test_pia_channel(void)
 {
-  point_t a[] = {{1, 7}, {4, 7}, {4, 6}, {2, 6}, {2, 3}, {4, 3}, {4, 2}, {1, 2}};
-  point_t b[] = {{3, 1}, {5, 1}, {5, 4}, {3, 4}, {3, 5}, {6, 5}, {6, 0}, {3, 0}};
-  size_t na = sizeof(a)/sizeof(point_t);
-  size_t nb = sizeof(b)/sizeof(point_t);
+  point_t
+    a[] = {{1, 7}, {4, 7}, {4, 6}, {2, 6}, {2, 3}, {4, 3}, {4, 2}, {1, 2}},
+    b[] = {{3, 1}, {5, 1}, {5, 4}, {3, 4}, {3, 5}, {6, 5}, {6, 0}, {3, 0}};
+  size_t
+    na = sizeof(a)/sizeof(point_t),
+    nb = sizeof(b)/sizeof(point_t);
   float area = pia_area(a, na, b, nb);
 
-  CU_ASSERT(area == 0.0f);
+  CU_ASSERT_EQUAL(area, 0.0f);
 }
 
 extern void test_pia_bowtie(void)
 {
-  point_t a[] = {{1, 1}, {1, 2}, {2, 1}, {2, 2}};
-  point_t b[] = {{0, 0}, {0, 4}, {4, 4}, {4, 0}};
-  size_t na = sizeof(a)/sizeof(point_t);
-  size_t nb = sizeof(b)/sizeof(point_t);
+  point_t
+    a[] = {{1, 1}, {1, 2}, {2, 1}, {2, 2}},
+    b[] = {{0, 0}, {0, 4}, {4, 4}, {4, 0}};
+  size_t
+    na = sizeof(a)/sizeof(point_t),
+    nb = sizeof(b)/sizeof(point_t);
   float area = pia_area(a, na, b, nb);
 
-  CU_ASSERT(area == 0.0f);
+  CU_ASSERT_EQUAL(area, 0.0f);
 }
 
 extern void test_pia_self_intersect(void)
 {
-  point_t a[] = {{0, 0}, {3, 0}, {3, 2}, {1, 2}, {1, 1}, {2, 1}, {2, 3}, {0, 3}};
-  point_t b[] = {{0, 0}, {0, 4}, {4, 4}, {4, 0}};
-  size_t na = sizeof(a)/sizeof(point_t);
-  size_t nb = sizeof(b)/sizeof(point_t);
+  point_t
+    a[] = {{0, 0}, {3, 0}, {3, 2}, {1, 2}, {1, 1}, {2, 1}, {2, 3}, {0, 3}},
+    b[] = {{0, 0}, {0, 4}, {4, 4}, {4, 0}};
+  size_t
+    na = sizeof(a)/sizeof(point_t),
+    nb = sizeof(b)/sizeof(point_t);
   float area = pia_area(a, na, b, nb);
 
-  CU_ASSERT(area == -9.0f);
+  CU_ASSERT_EQUAL(area, -9.0f);
 }
 
 extern void test_pia_half_rect(void)
 {
-  point_t a[] = {{0, 0}, {1, 0}, {0, 1}};
-  point_t b[] = {{0, 0}, {0, 1}, {1, 1}, {1, 0}};
-  size_t na = sizeof(a)/sizeof(point_t);
-  size_t nb = sizeof(b)/sizeof(point_t);
+  point_t
+    a[] = {{0, 0}, {1, 0}, {0, 1}},
+    b[] = {{0, 0}, {0, 1}, {1, 1}, {1, 0}};
+  size_t
+    na = sizeof(a)/sizeof(point_t),
+    nb = sizeof(b)/sizeof(point_t);
   float area = pia_area(a, na, b, nb);
 
-  CU_ASSERT(area == -0.5f);
+  CU_ASSERT_EQUAL(area, -0.5f);
 }
 
 extern void test_pia_cross(void)
 {
-  point_t a[] = {{1, 3}, {2, 3}, {2, 0}, {1, 0}};
-  point_t b[] = {{0, 1}, {3, 1}, {3, 2}, {0, 2}};
-  size_t na = sizeof(a)/sizeof(point_t);
-  size_t nb = sizeof(b)/sizeof(point_t);
+  point_t
+    a[] = {{1, 3}, {2, 3}, {2, 0}, {1, 0}},
+    b[] = {{0, 1}, {3, 1}, {3, 2}, {0, 2}};
+  size_t
+    na = sizeof(a)/sizeof(point_t),
+    nb = sizeof(b)/sizeof(point_t);
   float area = pia_area(a, na, b, nb);
-  //printf("[%.8f]", area);
-  CU_ASSERT(area == -1.0f);
+
+  CU_ASSERT_EQUAL(area, -1.0f);
 }
 
 extern void test_pia_drop_shadow(void)
 {
-  point_t a[] ={{0, 0}, {0, 2}, {2, 2}, {2, 0}};
-  point_t b[] = {{1, 1}, {3, 1}, {3, 3}, {1, 3}};
-  size_t na = sizeof(a)/sizeof(point_t);
-  size_t nb = sizeof(b)/sizeof(point_t);
+  point_t
+    a[] = {{0, 0}, {0, 2}, {2, 2}, {2, 0}},
+    b[] = {{1, 1}, {3, 1}, {3, 3}, {1, 3}};
+  size_t
+    na = sizeof(a)/sizeof(point_t),
+    nb = sizeof(b)/sizeof(point_t);
   float area = pia_area(a, na, b, nb);
-  //printf("[%.8f]", area);
-  CU_ASSERT(area == -1.0f);
+
+  CU_ASSERT_EQUAL(area, -1.0f);
 }
 
 extern void test_pia_rect_in_rect(void)
 {
-  point_t a[] = {{0, 0}, {0, 4}, {4, 4}, {4, 0}};
-  point_t b[] = {{1, 1}, {1, 2}, {2, 2}, {2, 1}};
-  size_t na = sizeof(a)/sizeof(point_t);
-  size_t nb = sizeof(b)/sizeof(point_t);
+  point_t
+    a[] = {{0, 0}, {0, 4}, {4, 4}, {4, 0}},
+    b[] = {{1, 1}, {1, 2}, {2, 2}, {2, 1}};
+  size_t
+    na = sizeof(a)/sizeof(point_t),
+    nb = sizeof(b)/sizeof(point_t);
   float area = pia_area(a, na, b, nb);
 
-  CU_ASSERT(area == 1.0f);
+  CU_ASSERT_EQUAL(area, 1.0f);
 }
